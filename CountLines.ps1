8add822056188b42f90b367975017e8b38e75862
Dan Brighton <daniel.brighton.00@gmail.com>
2025-03-20 17:44:54 +1100

    added exclusions

--- CountLines.ps1
+++ CountLines.ps1
@@ -8,9 +8,23 @@ if (-not (Test-Path $Path)) {
     exit 1
 }
 
+$excludedExtensions = @(
+    ".jpg", ".png", ".jpeg", ".gif", ".bmp", ".tiff",
+    ".pdf", ".webp", ".img",
+    ".exe",
+    ".mp3", ".mp4", ".avi", ".mov", ".wmv", ".mkv", ".flv",
+    ".wav", ".aac", ".ogg", ".flac",
+    ".zip", ".rar", ".7z", ".tar", ".gz", ".bz2",
+    ".dll", ".so", ".bin", ".iso",
+    ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx"
+)
+
 Write-Host "Counting lines of code in repository at: $Path`n"
 
-$files = Get-ChildItem -Path $Path -Recurse -File | Where-Object { $_.FullName -notmatch "\\\.git\\" }
+$files = Get-ChildItem -Path $Path -Recurse -File | Where-Object { 
+    $_.FullName -notmatch "\\\.git\\" -and 
+    $excludedExtensions -notcontains $_.Extension.ToLower()
+}
 $totalLines = 0
 
 foreach ($file in $files) {
